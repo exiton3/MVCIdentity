9a48b989a5d95602ccded7d7325ee6ba29789339
Sergey Kichuk <exiton3@gmail.com>
2017-02-09 12:51:39 +0300

    Create ReadExcel.cs

---
+++ Usefull/ReadExcel.cs
@@ -0,0 +1,45 @@
+    static void ReadAllCellValues(string fileName)
+        {
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                int i = 0;
+                foreach (WorksheetPart worksheetPart in workbookPart.WorksheetParts)
+                {
+                    Console.WriteLine(i++);
+                    OpenXmlReader reader = OpenXmlReader.Create(worksheetPart);
+
+                    while (reader.Read())
+                    {
+                        if (reader.ElementType == typeof(Row))
+                        {
+                            reader.ReadFirstChild();
+
+                            do
+                            {
+                                if (reader.ElementType == typeof(Cell))
+                                {
+                                    Cell c = (Cell)reader.LoadCurrentElement();
+
+                                    string cellValue;
+
+                                    if (c.DataType != null && c.DataType == CellValues.SharedString)
+                                    {
+                                        SharedStringItem ssi = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(int.Parse(c.CellValue.InnerText));
+
+                                        cellValue = ssi.Text.Text;
+                                    }
+                                    else
+                                    {
+                                        cellValue = c.CellValue.InnerText;
+                                    }
+
+                                    Console.Out.Write("{0}: {1} ", c.CellReference, cellValue);
+                                }
+                            } while (reader.ReadNextSibling());
+                            Console.Out.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
